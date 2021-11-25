ac87bad6c443352e91491567cb57080793ea86e4
joelcg <77513886+joelcg@users.noreply.github.com>
2021-11-25 10:18:20 +0700

    Simple GUI using kivy

---
+++ kivyGUI.py
@@ -0,0 +1,25 @@
+import kivy
+kivy.require('1.0.6')
+
+from kivy.uix.label import Label
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class MyApp(App):
+    
+    def build(self):
+        return Label(text='Hello World')
+
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+      
+class LoginScreen(GridLayout):
+    def __init__(self, **var_args):
+        
+        super(LoginScreen, self).__init__(**var_args)
+        self.cols = 2
+        self.add_widget(Label(text ='User Name'))
+        self.username = TextInput(multiline = True)
+        self.add_widget(self.username)
+        
